307c69665e6d6e2a430194d2df429f2bb725c3dc
raphael2618 <boussidan.raphael@gmail.com>
2026-03-06 14:18:22 +0200

    new

--- extractor/urls.py
+++ extractor/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.upload_invoice, name='upload_invoice'),
+    path('', views.upload_invoice, name='upload_invoice'), # Route pour la page principale
     path('export/', views.export_excel, name='export_excel'),
 ]
